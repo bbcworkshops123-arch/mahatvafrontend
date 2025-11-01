a12e0d23cf3bebcb9526b86a8bc89bb68a455cac
bbcworkshops123-arch <bbcworkshops123@gmail.com>
2025-11-01 17:48:55 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,10 @@ const Navbar = () => {
 
         <h2 className="nav-logo">
           <Link to="/">BALLARI BUSINESS COLLEGE</Link>
+          <h6 className="ad"> Recognised by Govt of Karnataka, Affiliated to Vijayanagara Sri Krishnadevaraya University Ballari, Approved by AICTE New Delhi and Recognized under Sec.2(f) of UGC Act of 1956.</h6>
+
         </h2>
+        
         <div className="nav-links">
           <Link to="/register">Register</Link>
           <Link to="/admin">Admin</Link>
